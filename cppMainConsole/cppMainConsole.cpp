eda2ced69f87f37fb105b3bca504661d13b3c534
Caojun Ma <cma910408@gmail.com>
2020-05-19 23:27:29 -0400

    Adding more projects

--- cppMainConsole/cppMainConsole.cpp
+++ cppMainConsole/cppMainConsole.cpp
@@ -6,6 +6,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -18,3 +20,12 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+int a = 5;
+int &r = a;
+
+int b[10];
+int& r1 = b[10];
+
+
