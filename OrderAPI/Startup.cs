0344aeb72c925bb74ad496b4a0b2e8b5d5acc061
Caojun Ma <cma910408@gmail.com>
2020-07-10 00:21:25 -0400

    Added orderapi project

--- OrderAPI/Startup.cs
+++ OrderAPI/Startup.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace EmailServices
+namespace OrderAPI
 {
     public class Startup
     {
